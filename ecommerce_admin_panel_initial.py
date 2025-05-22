540570d58570ab7ba4cd7685e44263cff0b672e3
rehab2d <r03007847@gmail.com>
2025-05-22 20:36:19 +0300

    add menu function and finalize program flow

--- ecommerce_admin_panel_initial.py
+++ ecommerce_admin_panel_initial.py
@@ -34,3 +34,29 @@ def delete_product():
             print(" Product ID not found.")
     except ValueError:
         print("Invalid ID format.")
+
+
+
+def menu():
+    while True:
+        print("\n=== Admin Panel Menu ===")
+        print("1. Add Product")
+        print("2. List Products")
+        print("3. Delete Product")
+        print("4. Exit")
+
+        choice = input("Enter choice: ")
+        if choice == "1":
+            add_product()
+        elif choice == "2":
+            list_products()
+        elif choice == "3":
+            delete_product()
+        elif choice == "4":
+            print(" Exiting...")
+            break
+        else:
+            print(" Invalid option.")
+
+if __name__ == "__main__":
+    menu()
